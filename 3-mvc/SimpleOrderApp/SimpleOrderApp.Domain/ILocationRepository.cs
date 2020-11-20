be938a07bbdf803c796ff4b46ed11d3d561dbec2
Nick Escalona <nicholas.escalona@gmail.com>
2020-11-20 11:36:22 -0600

    add simpleorderapp (more elaborate simplestore)

---
+++ 3-mvc/SimpleOrderApp/SimpleOrderApp.Domain/ILocationRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace SimpleOrderApp.Domain
+{
+    public interface ILocationRepository
+    {
+        IEnumerable<Location> GetAll();
+
+        void Create(Location location);
+
+        void Update(Location location);
+    }
+}
