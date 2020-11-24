224edf88f463a90cdcb726e187645f4806feb6be
Nick Escalona <nicholas.escalona@gmail.com>
2020-11-24 11:38:46 -0600

    restaurant reviews v3

---
+++ 3-mvc/RestaurantReviews/RestaurantReviews.DataAccess/Migrations/20201124173104_SomeChanges.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestaurantReviews.DataAccess.Migrations
+{
+    public partial class SomeChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Review_Restaurant_RestaurantId",
+                table: "Review");
+
+            migrationBuilder.EnsureSchema(
+                name: "RR");
+
+            migrationBuilder.RenameTable(
+                name: "Review",
+                newName: "Review",
+                newSchema: "RR");
+
+            migrationBuilder.RenameTable(
+                name: "Restaurant",
+                newName: "Restaurant",
+                newSchema: "RR");
+
+            migrationBuilder.RenameColumn(
+                name: "RestaurantId",
+                schema: "RR",
+                table: "Review",
+                newName: "RestaurantID");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                schema: "RR",
+                table: "Review",
+                newName: "ID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Review_RestaurantId",
+                schema: "RR",
+                table: "Review",
+                newName: "IX_Review_RestaurantID");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                schema: "RR",
+                table: "Restaurant",
+                newName: "ID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Review_Restaurant",
+                schema: "RR",
+                table: "Review",
+                column: "RestaurantID",
+                principalSchema: "RR",
+                principalTable: "Restaurant",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Review_Restaurant",
+                schema: "RR",
+                table: "Review");
+
+            migrationBuilder.RenameTable(
+                name: "Review",
+                schema: "RR",
+                newName: "Review");
+
+            migrationBuilder.RenameTable(
+                name: "Restaurant",
+                schema: "RR",
+                newName: "Restaurant");
+
+            migrationBuilder.RenameColumn(
+                name: "RestaurantID",
+                table: "Review",
+                newName: "RestaurantId");
+
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Review",
+                newName: "Id");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Review_RestaurantID",
+                table: "Review",
+                newName: "IX_Review_RestaurantId");
+
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Restaurant",
+                newName: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Review_Restaurant_RestaurantId",
+                table: "Review",
+                column: "RestaurantId",
+                principalTable: "Restaurant",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ 3-mvc/RestaurantReviews/RestaurantReviews.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using RestaurantReviews.Domain.Model;
+using RestaurantReviews.WebUI.ViewModels;
+
+namespace RestaurantReviews.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            TempData["currentUser"] = "Nick";
+            return View();
+        }
+
+        // GET: Privacy (not Home/Privacy)
+        [Route("privacy")]
+        //[HttpPost("privacy")] // sets route AND limits to POST
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
