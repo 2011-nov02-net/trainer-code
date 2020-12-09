2d0e54b537271b22202cb8786ebf0129e4f7f157
Nick Escalona <nicholas.escalona@gmail.com>
2020-12-09 10:26:28 -0600

    add console app with generated client code

---
+++ 5-web-services/WcfClient/WcfClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WcfClient.MyWcfService;
+
+namespace WcfClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CompositeType result;
+            using (var client = new Service1Client())
+            {
+                result = client.GetDataUsingDataContract(new CompositeType { append = true, StringValue = "adsf" });
+            }
+            Console.WriteLine(result.StringValue);
+        }
+    }
+}
