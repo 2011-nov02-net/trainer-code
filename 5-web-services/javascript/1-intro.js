48538c7de50089274ff935ee30d31b574423eb53
Nick Escalona <nicholas.escalona@gmail.com>
2020-12-11 11:01:26 -0600

    js environment

---
+++ 5-web-services/javascript/1-intro.js
@@ -0,0 +1,5 @@
+// JavaScript is weakly-typed object-based language
+// originally made for giving dynamic behavior to webpages
+// and now used in that role as well as many others
+
+console.log('Hello world')
