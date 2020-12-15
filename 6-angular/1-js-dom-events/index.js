2d93c3c434f3524c153448178ee37ca5d2a54a3c
Nick Escalona <nicholas.escalona@gmail.com>
2020-12-15 11:58:34 -0600

    js

--- 6-angular/1-js-dom-events/index.js
+++ 6-angular/1-js-dom-events/index.js
@@ -28,6 +28,17 @@ function start() {
 document.addEventListener('DOMContentLoaded', () => {
   //   document.getElementsByTagName('ul')[0];
   //   document.body.children[2];
+  // const link = document.getElementsByTagName('a')[0];
+  document.addEventListener('click', event => {
+    if (event.target.tagName === 'A') {
+      console.log('asdf');
+      event.preventDefault();
+      // why would i prevent default
+      // to implement single-page behavior for links
+      // or for forms
+    }
+  }, true);
+
   const list = document.getElementById('list');
 
   const button = document.createElement('button');
@@ -43,7 +54,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
   document.body.appendChild(button);
 
+  list.addEventListener('click', event => {
+    for (let node = event.target; node.parentNode != null; node = node.parentNode) {
+      if (node.tagName === 'LI') {
+        list.removeChild(node);
+        break;
+      }
+    }
+  });
+
   document.addEventListener('click', printEventDetails);
+
+  document.addEventListener('click', event => {
+    event.stopPropagation();
+  }, true);
   // button.addEventListener('click', printEventDetails);
   // list.addEventListener('click', printEventDetails);
 });
@@ -63,3 +87,15 @@ function printEventDetails(event) {
 }
 
 // if the user clicks on a list item, it should delete that list item.
+
+
+//-----
+
+// a simple web site with two HTML pages
+// a login page and a welcome page
+
+// the login page should have a form for username & password
+// if the user submits the form with
+// the correct credentials (hardcode these in JS)
+//   then navigate the user automatically to the welcome page.
+// otherwise, display an error message on the page.
